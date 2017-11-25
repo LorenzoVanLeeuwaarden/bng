604fbe0bc901f55e666cef4e71297a287319256b
Michael Trouw <michahell@soverin.net>
2017-11-25 18:53:11 +0100

    MVP done lel kek.

--- bng.js
+++ bng.js
@@ -1,4 +1,33 @@
 
 $( document ).ready(function() {
-	console.log( "ready!" );
+	console.log('document ready!');
+
+	// setup
+	$('input').iCheck({
+		checkboxClass: 'icheckbox_flat',
+		radioClass: 'iradio_flat'
+	});
+
+	// load names
+	let names;
+
+	$.get('names.json', function( theNames ) {
+		names = theNames;
+		console.log('male first names:', names.male.first);
+	});
+
+	// getRandomNamePart ('male', 'first')
+	const getRandomNamePart = (gender, firstOrLast) => {
+		let namePart = names[gender][firstOrLast];
+		return namePart[Math.round(Math.random() * (namePart.length - 1))];
+	};
+
+	$('button').on('click', {}, () => {
+		console.log('button clicked!');
+
+		let firstName = getRandomNamePart('male', 'first');
+		let lastName = getRandomNamePart('male', 'last');
+
+		$('#bng').html(firstName + lastName);
+	})
 });
