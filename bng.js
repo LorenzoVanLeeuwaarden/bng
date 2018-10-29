1701aecc9514b93dd4289f329b8a8c66ca8c8dd0
Michael Trouw <michahell@soverin.net>
2018-10-29 21:30:21 +0100

    some v3 shit right there

--- bng.js
+++ bng.js
@@ -24,6 +24,24 @@ $( document ).ready(function() {
 		return namePart[Math.round(Math.random() * (namePart.length - 1))];
 	};
 
+	let animateNewName = () => {
+		// Wrap every letter in a span
+		$('.ml6 .letters').each(function(){
+			$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+		});
+
+		anime.timeline({loop: false})
+		.add({
+			targets: '.ml6 .letter',
+			translateY: ["1.1em", 0],
+			translateZ: 0,
+			duration: 750,
+			delay: function(el, i) {
+				return 50 * i;
+			}
+		});
+	};
+
 	$('button').on('click', {}, () => {
 		// which gender was selected ?
 		let gender = $("#male").prop('checked')
@@ -34,7 +52,9 @@ $( document ).ready(function() {
 		let firstName = getRandomNamePart(gender, 'first');
 		let lastName = getRandomLastName();
 
-		let fullName = firstName + ' ' + lastName;
-		$('#bng').html(fullName);
-	})
+		// let fullName = firstName + ' ' + lastName;
+		$('#wngFirst').html(firstName);
+		$('#wngLast').html(lastName);
+		animateNewName();
+	});
 });
